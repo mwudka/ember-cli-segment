c8907f6ca8fbe3f865406fd79297c54bcdf1e051
Manuel Wudka-Robles <mwudka@gmail.com>
2023-01-23 20:51:29 -0800

    WIP

--- index.js
+++ index.js
@@ -24,9 +24,11 @@ module.exports = {
 
     segmentDomain += config.segment.snippetURL;
 
+    window._segmentSettings = config.segment.settings;
+
       return `<script ${nonceAttr} type="text/javascript">
           !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="${config.segment.snippetURL}";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="${config.segment.WRITE_KEY}";analytics.SNIPPET_VERSION="4.13.2";
-          analytics.load("${config.segment.WRITE_KEY}", config.segment.settings);
+          analytics.load("${config.segment.WRITE_KEY}", window._segmentSettings);
           }}();
         </script>`;
     }
